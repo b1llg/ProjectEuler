f110e204e8e4c29569169dbadb7e012132e51209
b1llg <guy.billy@outlook.com>
2025-03-27 20:36:41 -0400

    dev 04 wip

--- 04_LargestPalindromeProduct/functions.f90
+++ 04_LargestPalindromeProduct/functions.f90
@@ -1,21 +1,41 @@
 module Functions
 implicit none
 
-! private
+private
 
-! public
+public ispalyndrome
 
 contains
 
 logical function ispalyndrome(num) result(res)
    integer, intent(in) :: num
-   integer :: copy, reversed
+   integer :: copy, reversed, temp, count
 
+   ! Initialized result
+   res = .false.
    copy = num
+   count = 0
+   reversed = 0
 
-   do while(mod(copy, 10) == 0)
-    ! * wip  
+   do while(copy .ne. 0)
+      temp = mod(copy, 10)
+
+      if (temp == 0) then
+         copy = copy / 10
+         reversed = count*reversed
+         count = count + 1
+      else
+         reversed = 10*reversed + temp
+         copy = (copy - temp)/10
+         count = count + 1
+   
+      end if
+
    end do
+
+   if (reversed == num) then
+      res = .true.
+   end if
 
 
 end function ispalyndrome
--- 04_LargestPalindromeProduct/main.f90
+++ 04_LargestPalindromeProduct/main.f90
@@ -1,6 +1,21 @@
 program main
-   use Functions
+   use Functions, only: ispalyndrome
    implicit none
+   integer :: i,j, num
+
+   num = 100*101
+   print*,ispalyndrome(100*101)
+
+   ! ! Main loop
+   ! do i=100, 999
+   !    do j = i+1, 999
+   !       print*,i,j
+   !       if (ispalyndrome(i*j)) then
+   !          write(*,*) i,j,i*j
+   !       end if
+   !    end do
+   ! end do
+
 
    
 end program main
